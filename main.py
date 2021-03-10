04151a62be60fcbef12a45d20d44224d8f775189
kshah923 <k.shah923@gmail.com>
2021-03-09 22:07:22 -0500

    Complete (3-9-2021)
    Assignment Complete as of 3-9-2021.

--- main.py
+++ main.py
@@ -2,4 +2,32 @@
 # Webscraper Assignment
 # March 2021
 
-print("Hello, World!")
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+url = "http://18.206.38.144:8000/random_company"
+
+count = []
+df = pd.DataFrame(count, columns=['Name', 'Purpose'])
+
+for i in range(50):
+    r = requests.get(url=url)
+    soup = BeautifulSoup(r.text, features="html.parser")
+
+    x = soup.find('ol')
+    text = list(x.stripped_strings)
+
+    name = filter(lambda a: 'Name:' in a, text)
+    purpose = filter(lambda a: 'Purpose:' in a, text)
+    a = list(name)
+    a1 = str(a)[8:-2]
+    b = list(purpose)
+    b1 = str(b)[11:-2]
+
+    df = df.append({'Name': a1, 'Purpose': b1}, ignore_index=True)
+    df.index = np.arange(1, len(df) + 1)
+
+print(df)
+# df.to_csv('Webscraper.csv')
